04fe71c7bc0127bd14d9b0a634930ead23c3eebb
Eduardo <a33032@esas.pt>
2021-04-26 10:18:07 +0100

    js u

--- scripts.js
+++ scripts.js
@@ -1,16 +1,16 @@
 let alunos2P =["Anton","Rafael","Bruno","David","Diogo"];
 
-let uAlunos = document.getElementById("lista-alunos");
+let ulAlunos = document.getElementById("lista-alunos");
 
 for(let index =0;index<alunos2P.length;index++)
 {
     //buscar aluno ao array
-    const aluno = alunos2p[index];
+    const aluno = alunos2P[index];
     //Criar o <li>
     let liAlunos = documento.createElement("li");
     // adicionar o nome do aluno ao <li>
     liAlunos.innerText=aluno;
 
     //Adicional o <li> ao <ul>
-     uAlunos.append(liAlunos);
+     ulAlunos.append(liAlunos);
 }
