8054e986570072b0447a1572743d98e3fc6dbd9f
Eduardo <a33032@esas.pt>
2021-04-26 09:53:18 +0100

    atualkização do script

---
+++ scripts.js
@@ -0,0 +1,14 @@
+let alunos2P =["Anton","Rafael","Bruno","David","Diogo"];
+
+let uAlunos = document.getElementById("lista-alunos");
+
+for(let index =0;index<alunos2P.length;index++)
+{
+    //buscar aluno ao array
+    const aluno = alunos2p[index];
+    //Criar o <li>
+    let liAlunos = documento.createElement("li");
+
+    //Adicional o <li> ao <ul>
+    uAlunos.append(liAlunos);
+}
