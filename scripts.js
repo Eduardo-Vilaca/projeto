a902ce4a4393460cf9fac03a56f3ece3a2c877d3
Eduardo-Vilaca <a33032@esas.pt>
2021-05-03 08:42:26 +0100

    alteracoes

--- scripts.js
+++ scripts.js
@@ -4,13 +4,10 @@ let ulAlunos = document.getElementById("lista-alunos");
 
 for(let index =0;index<alunos2P.length;index++)
 {
-    //buscar aluno ao array
     const aluno = alunos2P[index];
-    //Criar o <li>
     let liAlunos = documento.createElement("li");
-    // adicionar o nome do aluno ao <li>
+
     liAlunos.innerText=aluno;
 
-    //Adicional o <li> ao <ul>
      ulAlunos.append(liAlunos);
 }
