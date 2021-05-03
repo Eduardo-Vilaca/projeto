48b0e5b315eb68adafe1cb9141c8532f0324c134
Eduardo-Vilaca <a33032@esas.pt>
2021-05-03 09:27:50 +0100

    Acabado. Close #5.

--- scripts.js
+++ scripts.js
@@ -13,11 +13,11 @@ for(let index=0;index<alunos2P.length;index++)
      ulAlunos.append(liAlunos);
 }
 
-let disciplinas2P =["PSI","RC","SO","MAT","FQ"];
+let disciplinas2P =["PSI","RC","SO","MAT","FQ", "PT"];
 
 let ulDisciplinas = document.getElementById("lista-disciplinas");
 
-for(let index=0;index<alunos2P.length;index++)
+for(let index=0;index<disciplinas2P.length;index++)
 {
     const disciplina = disciplinas2P[index];
     let liDisciplinas = document.createElement("li");
@@ -26,3 +26,22 @@ for(let index=0;index<alunos2P.length;index++)
     
     ulDisciplinas.append(liDisciplinas);
 }
+let tbody = document.getElementById("tabelabody");
+let writetr = document.createElement("tr");
+let writetr2 = document.createElement("tr");
+
+for(let index=0;index<disciplinas2P.length;index++)
+{
+    const disciplina = disciplinas2P[index];
+    let writetd = document.createElement("td");
+    writetd.innerText=disciplina;
+
+    if(index > 2) {
+        writetr2.append(writetd);
+    }else{
+
+        writetr.append(writetd);
+    }
+}
+tbody.append(writetr);
+tbody.append(writetr2);
