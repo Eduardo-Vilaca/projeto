878c42373fa50b96b19e0326747c28e445321bd3
Eduardo-Vilaca <a33032@esas.pt>
2021-05-03 08:53:01 +0100

    Alunos e Disciplinas done

--- scripts.js
+++ scripts.js
@@ -1,13 +1,28 @@
+
 let alunos2P =["Anton","Rafael","Bruno","David","Diogo"];
 
 let ulAlunos = document.getElementById("lista-alunos");
 
-for(let index =0;index<alunos2P.length;index++)
+for(let index=0;index<alunos2P.length;index++)
 {
     const aluno = alunos2P[index];
-    let liAlunos = documento.createElement("li");
+    let liAlunos = document.createElement("li");
 
     liAlunos.innerText=aluno;
-
+    
      ulAlunos.append(liAlunos);
+}
+
+let disciplinas2P =["PSI","RC","SO","MAT","FQ"];
+
+let ulDisciplinas = document.getElementById("lista-disciplinas");
+
+for(let index=0;index<alunos2P.length;index++)
+{
+    const disciplina = disciplinas2P[index];
+    let liDisciplinas = document.createElement("li");
+
+    liDisciplinas.innerText=disciplina;
+    
+    ulDisciplinas.append(liDisciplinas);
 }
